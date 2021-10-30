2572b0491ab5f9d5d1509f9fcf62502c46bad4c8
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-10-30 13:35:31 +0200

    Implementar una sonda de temperatura

---
+++ src/AutoAdaptativeSystem/TemperatureProbe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TemperatureProbe
+{
+    using System.Net.Http;
+    using System.Threading;
+    using RoomMonitor.ApiClient.Api;
+    using RoomMonitor.ApiClient.Client;
+    using RoomMonitor.ApiClient.Model;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeasurementApi measurementApi = new MeasurementApi("http://localhost:5000");
+
+            Random random = new Random();
+
+            while (true)
+            {
+                measurementApi.MeasurementTemperaturePost(new TemperatureMeasurementDTO(random.NextDouble() * 35, TemperatureUnit.NUMBER_1));
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
