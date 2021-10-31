31e7798e1edc3de817773c59c37a7bf97b1b3882
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-10-31 08:22:44 +0100

    Refactorizar el script de generar API Client en Python

---
+++ src/AutoAdaptativeSystem/GenerateApiClient.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+from io import FileIO
+from pathlib import Path
+import os
+import requests
+
+codegen_library_version = "5.3.0"
+codegen_library_name = "openapi-generator-cli-5.3.0.jar" 
+
+def generate_api_spec(project_name, project_path, api_name):
+    print(f"Building '{project_path}'...")
+    result = os.system(f"dotnet build {project_path} -c Release")
+
+    if result != 0:
+        return False
+
+    openapi_spec = f"{project_name}.json"
+
+    print(f"Generating OpenApi spec of '{project_path}'...")
+
+    dll_path = Path(project_path).parent.joinpath(f"bin/Release/net5.0/{project_name}.dll")
+    result = os.system(f"swagger tofile --output {openapi_spec} \"{dll_path}\" \"{api_name}\"")
+
+    openapi_spec_path = Path(openapi_spec).absolute()
+    print(f"Generated '{openapi_spec_path}'")
+    
+    return openapi_spec_path
+
+def download_codegen_jar():
+    print(f"Downloading OpenAPI Codegen version '{codegen_library_version}'...")
+
+    if Path(codegen_library_name).exists():
+        return Path(codegen_library_name).absolute()
+    
+    codegen_jar_descriptor = requests.get(f"https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/{codegen_library_version}/{codegen_library_name}")
+    
+    fd = os.open(codegen_library_name, os.O_WRONLY | os.O_CREAT)
+    os.write(fd, codegen_jar_descriptor.content)
+
+    absolute_path = Path(codegen_library_name).absolute()
+    print(f"Downloaded file '{absolute_path}'...")
+
+    return absolute_path
+
+def generate_api_client(codegen_path, openapi_path, output_path):
+    print(f"Generating API Client from '{openapi_path}' in '{output_path}'...")
+
+    os.system(f"java -jar {codegen_path} generate -i {openapi_path} -o {output_path} -g csharp-netcore --library httpclient")
+
+project_list = [
+    { "path": "./RoomMonitor/RoomMonitor.csproj", "name": "RoomMonitor", "api_name": "v1", "output_path": "./RoomMonitor/ApiClient" },
+    { "path": "./MonitoringModule/MonitoringModule.csproj", "name": "MonitoringModule", "api_name": "v1", "output_path": "./MonitoringModule/ApiClient" }
+]
+
+codegen_path = download_codegen_jar()
+
+for project in project_list:
+    openapi_spec_path = generate_api_spec(project["name"], project["path"], project["api_name"])
+
+    generate_api_client(codegen_path, openapi_spec_path, project["output_path"])
