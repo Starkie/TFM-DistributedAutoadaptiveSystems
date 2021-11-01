39fcadde5bb2141478176264adfb94572545eec7
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-11-01 09:32:33 +0100

    Generar los enumerados como strings

--- src/AutoAdaptativeSystem/RoomMonitor/Startup.cs
+++ src/AutoAdaptativeSystem/RoomMonitor/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.OpenApi.Models;
 
 namespace RoomMonitor
 {
+    using System.Text.Json.Serialization;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,7 +28,10 @@ namespace RoomMonitor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RoomMonitor", Version = "v1" });
