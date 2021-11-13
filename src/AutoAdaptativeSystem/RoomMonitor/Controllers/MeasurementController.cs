a7ed01d72a65b8fca26a4533a31707d2c33fefd4
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-11-13 19:13:12 +0100

    Registrar las temperaturas en el MonitoringModule

--- src/AutoAdaptativeSystem/RoomMonitor/Controllers/MeasurementController.cs
+++ src/AutoAdaptativeSystem/RoomMonitor/Controllers/MeasurementController.cs
@@ -1,35 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace RoomMonitor.Controllers
 {
+    using System.Globalization;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Http;
+    using MonitoringModule.ApiClient.Api;
+    using MonitoringModule.ApiClient.Model;
     using RoomMonitor.DTOS;
+    using PropertyDTO = MonitoringModule.ApiClient.Model.PropertyDTO;
 
     [ApiController]
     [Route("[controller]")]
     public class MeasurementController : ControllerBase
     {
+        private const string Temperature = "Temperature";
+
         private readonly ILogger<MeasurementController> _logger;
 
-        public MeasurementController(ILogger<MeasurementController> logger)
+        private readonly MonitorApi _monitorApi;
+
+        private readonly PropertyApi _propertyApi;
+
+        private readonly Guid _monitorId = new Guid("92bb183c-c94f-41b4-b1b8-916fec3e5db8");
+
+        public MeasurementController(ILogger<MeasurementController> logger, MonitorApi monitorApi, PropertyApi propertyApi)
         {
-            this._logger = logger;
+            _logger = logger;
+            _monitorApi = monitorApi;
+            _propertyApi = propertyApi;
         }
 
         [HttpPost("Temperature")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult RegisterTemperatureMeasurement([FromBody]TemperatureMeasurementDTO temperatureMeasurementDto)
+        public async Task<IActionResult> RegisterTemperatureMeasurement([FromBody]TemperatureMeasurementDTO temperatureMeasurementDto)
         {
-            this._logger.LogInformation("[Temperature] - Received Measurement: {Value} {Unit}", temperatureMeasurementDto.Value, temperatureMeasurementDto.Unit);
+            _logger.LogInformation("[Temperature] - Received Measurement: {Value} {Unit}", temperatureMeasurementDto.Value, temperatureMeasurementDto.Unit);
 
-            // TODO: Implement Logic.
-            return this.Ok();
+            PropertyDTO propertyValue = await _propertyApi.PropertyPropertyNameGetAsync(Temperature);
+
+            if (propertyValue is not null)
+            {
+                _logger.LogInformation("[Temperature] - The previous value was: {PropertyValue}, Last modified in: {LastModificationDate}", propertyValue.Value, propertyValue.LastModification);
+            }
+
+            await _monitorApi.MonitorMonitorIdMeasurementPostAsync( _monitorId,  new MeasurementDTO()
+            {
+                ProbeId = Guid.NewGuid(),
+                Property = new Property
+                {
+                    Key = Temperature,
+                    Value = temperatureMeasurementDto.Value.ToString(CultureInfo.InvariantCulture),
+                },
+            }).ConfigureAwait(false);
+
+            return Ok();
         }
 
         [HttpPost("Humidity")]
@@ -40,7 +69,7 @@ namespace RoomMonitor.Controllers
             this._logger.LogInformation("[Humidity] - Received Measurement: {Value} {Unit}", humidityMeasurementDto.Value, humidityMeasurementDto.Unit);
 
             // TODO: Implement Logic.
-            return this.Ok();
+            return Ok();
         }
     }
 }
