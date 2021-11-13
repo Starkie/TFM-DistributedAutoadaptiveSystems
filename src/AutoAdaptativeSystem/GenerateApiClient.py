389a6428b9f3d467b31817887589082b7c904216
Adriano Vega Llobell <adrianovega@hotmail.com>
2021-11-13 19:14:55 +0100

    Extraer la versión de .NET Core a usar a una variable

--- src/AutoAdaptativeSystem/GenerateApiClient.py
+++ src/AutoAdaptativeSystem/GenerateApiClient.py
@@ -6,8 +6,7 @@ import os
 import requests
 import shutil
 
-codegen_library_version = "5.3.0"
-codegen_library_name = "openapi-generator-cli-5.3.0.jar" 
+netCoreVersion = "net6.0"
 
 def generate_api_spec(project_name, project_path, api_name):
     print(f"Building '{project_path}'...")
@@ -19,7 +18,7 @@ def generate_api_spec(project_name, project_path, api_name):
     print(f"Generating OpenApi spec of '{project_path}'...")
 
     parent_folder = Path(project_path).parent
-    dll_path = parent_folder.joinpath(f"bin/Release/net5.0/{project_name}.dll")
+    dll_path = parent_folder.joinpath(f"bin/Release/{netCoreVersion}/{project_name}.dll")
 
     openapi_spec_path = f"{project_name}-OpenAPISpec.json"
     openapi_spec_path = parent_folder.joinpath(openapi_spec_path).absolute()
@@ -49,7 +48,7 @@ def download_codegen_jar():
 def generate_api_client(codegen_path, openapi_path, project_name, output_path):
     print(f"Generating API Client from '{openapi_path}' in '{output_path}'...")
 
-    os.system(f"java -jar {codegen_path} generate -i {openapi_path} -o {output_path} -g csharp-netcore --library httpclient --additional-properties=packageName={project_name}.ApiClient,netCoreProjectFile=true,targetFramework=net5.0,sourceFolder=\"\"")
+    os.system(f"java -jar {codegen_path} generate -i {openapi_path} -o {output_path} -g csharp-netcore --library httpclient --additional-properties=packageName={project_name}.ApiClient,netCoreProjectFile=true,targetFramework={netCoreVersion},sourceFolder=\"\"")
 
 project_list = [
     { "path": "./KnowledgeService/KnowledgeService.csproj", "name": "KnowledgeService", "api_name": "v1", "output_path": "./KnowledgeService/ApiClient", "remove_existing_files": True },
