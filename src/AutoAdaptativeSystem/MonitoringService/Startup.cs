715b421271728288c50e013c06709a8594308740
Adriano Vega Llobell <adrianovega@hotmail.com>
2022-02-05 18:11:41 +0100

    Adaptar el servicio de monitorización

--- src/AutoAdaptativeSystem/MonitoringService/Startup.cs
+++ src/AutoAdaptativeSystem/MonitoringService/Startup.cs
@@ -1,9 +1,5 @@
 namespace MonitoringService;
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Text.Json.Serialization;
 using KnowledgeService.ApiClient.Api;
 using Microsoft.AspNetCore.Builder;
@@ -11,11 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using MonitoringService.Configurations;
 using MonitoringService.Diagnostics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 public class Startup
 {
@@ -33,39 +26,19 @@ public class Startup
             .AddJsonOptions(options =>
                 options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
+        services.AddSwagger("Monitoring Service", string.Empty, "v1");
+
+        services.AddTracing(MonitoringServiceConstants.AppName, "v1.0");
+
+        services.AddSingleton<MonitoringServiceDiagnostics>();
+
         services.AddScoped<IPropertyApi, PropertyApi>(_ =>
         {
-            KnowledgeServiceConfiguration configuration =
-                Configuration.GetSection(KnowledgeServiceConfiguration.ConfigurationPath)
-                    .Get<KnowledgeServiceConfiguration>();
+            var configuration =
+                Configuration.BindOptions<KnowledgeServiceConfiguration>(KnowledgeServiceConfiguration.ConfigurationPath);
 
             return new PropertyApi(configuration.ServiceUri);
         });
-
-        services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "MonitoringService", Version = "v1" });
-
-            // Set the comments path for the Swagger JSON and UI.
-            // Obtained from https://github.com/domaindrivendev/Swashbuckle.WebApi/issues/93#issuecomment-458690098.
-            List<string> xmlFiles = Directory.GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.TopDirectoryOnly)
-                .ToList();
-
-            xmlFiles.ForEach(xmlFile => c.IncludeXmlComments(xmlFile));
-        });
-
-        services.AddOpenTelemetryTracing(builder =>
-        {
-            builder.SetResourceBuilder(ResourceBuilder
-                    .CreateDefault()
-                    .AddService(MonitoringServiceConstants.AppName, serviceVersion: "ver1.0"))
-                .AddAspNetCoreInstrumentation()
-                .AddHttpClientInstrumentation()
-                .AddSource(MonitoringServiceConstants.AppName)
-                .AddJaegerExporter();
-        });
-
-        services.AddSingleton<MonitoringServiceDiagnostics>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,9 +47,10 @@ public class Startup
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MonitoringService v1"));
         }
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MonitoringService v1"));
 
         app.UseRouting();
 
