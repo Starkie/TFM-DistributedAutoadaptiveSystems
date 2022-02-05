0b30763dcc181ca2877c4fb371e50aef6cbe234a
Adriano Vega Llobell <adrianovega@hotmail.com>
2022-02-05 15:05:04 +0100

    Adaptar las variables  para recibir la configuración de Jaeger y Loki desde fuera

--- src/AutoAdaptativeSystem/KnowledgeService/Startup.cs
+++ src/AutoAdaptativeSystem/KnowledgeService/Startup.cs
@@ -1,21 +1,17 @@
+namespace KnowledgeService;
+
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using KnowledgeService.Diagnostics;
+using KnowledgeService.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
-namespace KnowledgeService;
-
-using System.IO;
-using KnowledgeService.Diagnostics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
@@ -31,15 +27,16 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
         services.AddControllers();
 
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
                 Title = "Knowledge Service",
                 Description = "Demonstrates all the existing operations to access and manage Knowledge properties.",
-                Version = "v1" });
+                Version = "v1",
+            });
 
             // Set the comments path for the Swagger JSON and UI.
             // Obtained from https://github.com/domaindrivendev/Swashbuckle.WebApi/issues/93#issuecomment-458690098.
@@ -57,7 +54,13 @@ public class Startup
                 .AddAspNetCoreInstrumentation()
                 .AddHttpClientInstrumentation()
                 .AddSource(KnowledgeServiceConstants.AppName)
-                .AddJaegerExporter();
+                .AddJaegerExporter(configure =>
+                {
+                    var jaegerOptions = Configuration.BindOptions<JaegerOptions>(JaegerOptions.ConfigurationPath);
+
+                    configure.AgentHost = jaegerOptions.Host;
+                    configure.AgentPort = jaegerOptions.Port;
+                });
         });
 
         services.AddSingleton<KnowledgeServiceDiagnostics>();
@@ -69,9 +72,10 @@ public class Startup
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KnowledgeService v1"));
         }
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KnowledgeService v1"));
 
         app.UseRouting();
 
