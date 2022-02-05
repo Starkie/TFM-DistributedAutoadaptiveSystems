a1260300dd1b85e295565dfd9f98d0241ab03bfd
Adriano Vega Llobell <adrianovega@hotmail.com>
2022-02-05 18:44:26 +0100

    Eliminar código muerto

--- src/AutoAdaptativeSystem/RoomMonitor/Controllers/MeasurementController.cs
+++ src/AutoAdaptativeSystem/RoomMonitor/Controllers/MeasurementController.cs
@@ -1,25 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace RoomMonitor.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
-namespace RoomMonitor.Controllers;
-
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Http;
-using MonitoringService.ApiClient.Api;
 using RoomMonitor.DTOS;
-using PropertyDTO = MonitoringService.ApiClient.Model.PropertyDTO;
 
 [ApiController]
 [Route("[controller]")]
 public class MeasurementController : ControllerBase
 {
-    private readonly ILogger<MeasurementController> _logger;
-
     private readonly IMediator _mediator;
-
-    private readonly RoomMonitorDiagnostics _roomMonitorDiagnostics;
-
+    
     public MeasurementController(IMediator mediator)
     {
         _mediator = mediator;
