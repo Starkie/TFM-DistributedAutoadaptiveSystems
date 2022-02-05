9bfdb2b3e5fe2b61de92d194da3761272f49497d
Adriano Vega Llobell <adrianovega@hotmail.com>
2022-02-05 18:12:14 +0100

    Implementar un docker compose y un script para compilar para que sea más sencillo levantar la solución

---
+++ src/AutoAdaptativeSystem/build.ps1
@@ -0,0 +1,45 @@
+function Build-Project([psobject]$Project, [string]$PublishPath)
+{
+    $PublishDirectory = Join-Path $PublishPath $Project.Name
+
+    mkdir $PublishDirectory
+
+    Write-Host $PublishDirectory
+
+    dotnet publish "$($Project.Path)/$($Project.Path).csproj" -c Release -r linux-x64 --no-self-contained -o "$PublishDirectory/app" -v m /p:clp=Summary -m
+
+    cp (Join-Path $Project.Path "Dockerfile") "$PublishDirectory/Dockerfile"
+}
+
+$Projects = @(
+    @{
+        Name = "knowledge"
+        Path = "KnowledgeService"
+    },
+    @{
+        Name = "monitoring"
+        Path = "MonitoringService"
+    },
+    @{
+        Name = "roommonitor"
+        Path = "RoomMonitor"
+    }
+)
+
+$PublishPath = Join-Path (Get-Location) "publish"
+
+if (Test-Path $PublishPath)
+{
+    Remove-Item -Recurse $PublishPath
+}
+
+mkdir $PublishPath
+
+foreach ($project in $Projects) {
+    Build-Project $project $PublishPath
+}
+
+cp "docker-compose.yml" "$PublishPath/docker-compose.yml"
+
+# Start the compose.
+docker-compose -f ./publish/docker-compose.yml up --build
