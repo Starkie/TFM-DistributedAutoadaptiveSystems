dfc1c51506d63b4d8aeb84122dc88f84d1236a6a
Adriano Vega Llobell <adrianovega@hotmail.com>
2022-02-19 13:44:20 +0100

    Registrar los handlers en base a las subscripciones

--- src/AutoAdaptativeSystem/Core/Core.Bus/Configurations/BusConfiguration.cs
+++ src/AutoAdaptativeSystem/Core/Core.Bus/Configurations/BusConfiguration.cs
@@ -5,4 +5,6 @@ public class BusConfiguration
     public const string ConfigurationPath = "BusConfiguration";
 
     public string ServiceUri { get; set; }
+
+    public string InputQueueName { get; set; }
 }
--- src/AutoAdaptativeSystem/Core/Core.Bus/Extensions/ServiceCollectionExtensions.cs
+++ src/AutoAdaptativeSystem/Core/Core.Bus/Extensions/ServiceCollectionExtensions.cs
@@ -4,32 +4,52 @@ using System.Reflection;
 using Core.Bus.Configuration;
 using Core.Bus.Publisher;
 using Microsoft.Extensions.Configuration;
+using Rebus.Bus;
 using Rebus.Config;
+using Rebus.Handlers;
 using Rebus.Routing;
 
 public static class ServiceCollectionExtensions
 {
     // TODO: Use a convention registry of the publishers and handlers.
     // TODO: Defer the connection to the bus with a background service.
-    public static IServiceCollection AddBus(this IServiceCollection services, IConfiguration configuration, Assembly assembly, Action<StandardConfigurer<IRouter>>? mapEventsAction = null)
+    public static IServiceCollection AddBus(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        Assembly assembly,
+        Func<IBus, Task>? registerSubscriptions = null)
     {
         var busOptions = configuration.BindOptions<BusConfiguration>(BusConfiguration.ConfigurationPath);
+
+        bool canReceiveMessages = !string.IsNullOrEmpty(busOptions.InputQueueName);
 
         services.AddRebus(configure =>
         {
             configure.Logging(l => l.Serilog())
                 .Options(o => o.EnableDiagnosticSources())
-                .Transport(t => t.UseRabbitMqAsOneWayClient(busOptions.ServiceUri));
+                .Transport(t =>
+                {
 
-            if (mapEventsAction != null)
-            {
-                configure.Routing(mapEventsAction);
-            }
+                    if (!canReceiveMessages)
+                    {
+                        t.UseRabbitMqAsOneWayClient(busOptions.ServiceUri);
+                    }
+                    else
+                    {
+                        t.UseRabbitMq(busOptions.ServiceUri, busOptions.InputQueueName);
+                    }
+                });
 
             return configure;
-        });
+        },
+            onCreated: registerSubscriptions);
 
         services.RegisterServicesFromAssembly(typeof(IIntegrationEventPublisher<>), assembly, lifetime: ServiceLifetime.Scoped);
+
+        if (canReceiveMessages)
+        {
+            services.RegisterServicesFromAssembly(typeof(IHandleMessages<>), assembly, lifetime: ServiceLifetime.Scoped);
+        }
 
         return services;
     }
