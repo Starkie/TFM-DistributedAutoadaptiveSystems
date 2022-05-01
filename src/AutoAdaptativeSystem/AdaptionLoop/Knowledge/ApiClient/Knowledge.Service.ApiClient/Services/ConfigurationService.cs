7e3b5ec3aea8399b1ea3cfa68fe7b8d5584aa310
Adriano Vega Llobell <adrianovega@hotmail.com>
2022-05-01 15:15:34 +0200

    Mejorar la deserialización de strings y enums

--- src/AutoAdaptativeSystem/AdaptionLoop/Knowledge/ApiClient/Knowledge.Service.ApiClient/Services/ConfigurationService.cs
+++ src/AutoAdaptativeSystem/AdaptionLoop/Knowledge/ApiClient/Knowledge.Service.ApiClient/Services/ConfigurationService.cs
@@ -1,9 +1,11 @@
 namespace Knowledge.Service.ApiClient.Services;
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Knowledge.Service.ApiClient.Api;
 using Knowledge.Service.ApiClient.Client;
+using Knowledge.Service.ApiClient.Model;
 using Newtonsoft.Json;
 
 public class ConfigurationService : IConfigurationService
@@ -28,7 +30,7 @@ public class ConfigurationService : IConfigurationService
         {
             var configuration = await _serviceConfigurationApi.ServiceServiceNameConfigurationConfigurationNameGetAsync(serviceName, configurationName, cancellationToken);
 
-            propertyValue = JsonConvert.DeserializeObject<T>(configuration.Value);
+            propertyValue = Deserialize<T>(configuration);
         }
         catch (ApiException exception)
         {
@@ -37,6 +39,40 @@ public class ConfigurationService : IConfigurationService
         catch (JsonSerializationException exception)
         {
             // _logger.LogWarning("Serialization error: Could not deserialize as '{Type}'", nameof(TemperatureMeasurementDTO));
+        }
+
+        return propertyValue;
+    }
+
+    private static T Deserialize<T>(ConfigurationDTO configuration)
+    {
+        if (configuration is null)
+        {
+            return default;
+        }
+
+        if (typeof(T) == typeof(string))
+        {
+            // Ew, boxing.
+            return (T)(object)configuration.Value;
+        }
+
+        T propertyValue;
+
+        var targetType = typeof(T);
+
+        if (targetType.IsGenericType)
+        {
+            targetType = targetType.GenericTypeArguments[0];
+        }
+
+        if (targetType.IsEnum)
+        {
+            propertyValue = (T)Enum.Parse(targetType, configuration.Value);
+        }
+        else
+        {
+            propertyValue = JsonConvert.DeserializeObject<T>(configuration.Value);
         }
 
         return propertyValue;
